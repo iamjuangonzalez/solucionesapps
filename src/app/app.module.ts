5e05a6d3856355428b1753e6b6dca654491138e7
Juan José González Dulcey <70113663+iamjuangonzalez@users.noreply.github.com>
2023-10-27 20:12:04 -0500

    add resolve module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { ContentHeroComponent } from './components/content-hero/content-hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ContentHeroComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
