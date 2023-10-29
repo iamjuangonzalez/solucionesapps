065f00714db75b3f6f4aad69d09d8dbb56f9b591
Shamir León <leonshamir1@gmail.com>
2023-10-28 21:28:24 -0500

    Add Hero Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { InfoCardsComponent } from './components/info-cards/info-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeroComponent,
+    InfoCardsComponent
   ],
   imports: [
     BrowserModule,
