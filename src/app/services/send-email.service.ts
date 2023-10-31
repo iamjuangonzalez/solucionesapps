2b278c3bef3c13be192494f8290e84af56e67f0f
Juan José González Dulcey <70113663+iamjuangonzalez@users.noreply.github.com>
2023-10-31 17:06:37 -0500

    resolve email

--- src/app/services/send-email.service.ts
+++ src/app/services/send-email.service.ts
@@ -8,13 +8,13 @@ export class SendEmailService {
 
   public send_email_true: boolean = false;
   public servide_id: string = 'service_wobwdhb';
-  public template_id: string = 'template_vjr8kcp';
+  public template_id_new_client: string = 'template_5nt17ya';
   public public_key: string = 'pEXBj6IANNiBs_lgX'
 
   constructor() { }
 
   sendEmail = (emailData: any) => {
-    emailjs.send(this.servide_id, this.template_id, emailData, this.public_key)
+    emailjs.send(this.servide_id, this.template_id_new_client, emailData, this.public_key)
       .then(
         result => {
           console.log(result.text)
