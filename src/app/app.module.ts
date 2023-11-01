5baa39a0a2778363531d0c066481e75a21e09ad5
Juan José González Dulcey <70113663+iamjuangonzalez@users.noreply.github.com>
2023-10-31 22:20:29 -0500

    add changes aliados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AdvantagesComponent } from './components/advantages/advantages.component';
 import { ServicesComponent } from './components/services/services.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AliadosComponent } from './components/aliados/aliados.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FooterComponent } from './components/footer/footer.component';
     NavbarComponent,
     AdvantagesComponent,
     ServicesComponent,
-    FooterComponent
+    FooterComponent,
+    AliadosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/aliados/aliados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aliados',
+  templateUrl: './aliados.component.html',
+  styleUrls: ['./aliados.component.scss']
+})
+export class AliadosComponent {
+
+}
